0a3655b7a9e4a619f30ef42e455831e0d6ad5eb7
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-09 19:13:23 +0000

    Updated output.tf

---
+++ modules/mod_resourcegrp/output.tf
@@ -0,0 +1,11 @@
+output "output_rg_name" {
+value=azurerm_resource_group.rg_new.name
+}
+
+output "output_rg_location" {
+value=azurerm_resource_group.rg_new.location
+}
+
+output "output_rg_id" {
+value=azurerm_resource_group.rg_new.location
+}
