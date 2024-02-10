983957d7be50cfe2bd33b5fbd2a535a356717ffd
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 06:04:34 +0000

    Updated main.tf

--- modules/mod_subnet/main.tf
+++ modules/mod_subnet/main.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_subnet" "st" {
   name                 = var.subnet_name
-  resource_group_name  = var.output_rg_name
-  virtual_network_name = var.output_vnet_name
+  resource_group_name  = module.mod_resourcegrp.rg_name
+  virtual_network_name = module.mod_virtualnetwork.vnet_name
   address_prefixes     = ["10.0.2.0/24"]
 }
