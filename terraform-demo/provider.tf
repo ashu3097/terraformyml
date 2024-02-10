10226c11ffb72c2d4c6544e3dcbeff9033d9298f
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 18:42:44 +0000

    Updated provider.tf

--- terraform-demo/provider.tf
+++ terraform-demo/provider.tf
@@ -6,4 +6,7 @@ provider "azurerm" {
   client_secret   = "iAt8Q~PdVKAomu~capCeOLShN0ANFEDX~MaV2doF"
   subscription_id = "4f5247e7-3a1f-4020-ae0b-900086d984bf"
   tenant_id       = "9f488269-cbdd-40e9-9bd4-f2be680481bc" # (Optional) If not specified, defaults to the tenant of the service principal
+  
+  features {}
+
 }
