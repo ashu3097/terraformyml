4015acc13055904563b3a4fecce14febc7771adc
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 17:05:59 +0000

    Updated provider.tf

--- terraform-demo/provider.tf
+++ terraform-demo/provider.tf
@@ -1,3 +1,10 @@
-provider "azurerm" {
-  features {}
-}
+terraform {
+ # required_version = "~> 1.3.4"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+    #  version = "3.22.0"
+     
+    }
+  }
