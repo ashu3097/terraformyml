445b567613b276b1c642225dd56bfa7050342b42
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 17:11:13 +0000

    Updated provider.tf

--- terraform-demo/provider.tf
+++ terraform-demo/provider.tf
@@ -1,10 +1,13 @@
 terraform {
- # required_version = "~> 1.3.4"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-    #  version = "3.22.0"
-     
+      version = "=3.0.0"
     }
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
