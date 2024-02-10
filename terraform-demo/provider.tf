26dde7c57074e5095852d97970144140b743461f
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 18:39:32 +0000

    Updated provider.tf

--- terraform-demo/provider.tf
+++ terraform-demo/provider.tf
@@ -1,14 +1,9 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
-
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
+  version = "~> 2.0"
+
+  # You can specify the Azure credentials directly in the provider block
+  client_id       = "8d1d889e-d8ba-4a70-b0a5-0043dbed8caf"
+  client_secret   = "iAt8Q~PdVKAomu~capCeOLShN0ANFEDX~MaV2doF"
   subscription_id = "4f5247e7-3a1f-4020-ae0b-900086d984bf"
-  features {}
+  tenant_id       = "9f488269-cbdd-40e9-9bd4-f2be680481bc" # (Optional) If not specified, defaults to the tenant of the service principal
 }
