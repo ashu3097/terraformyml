b2201bb56d182457002abc5c2f2723c550e1e78f
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 09:36:10 +0000

    Updated output.tf

--- modules/mod_virtualnetwork/output.tf
+++ modules/mod_virtualnetwork/output.tf
@@ -1,3 +1,7 @@
-output "output_vnet_name" {
-  value=azurerm_virtual_network.vnet.id
+output "output_nic_name" {
+  value=azurerm_network_interface.nic.name
+}
+
+output "output_nic_id" {
+  value=azurerm_network_interface.nic.id
 }
