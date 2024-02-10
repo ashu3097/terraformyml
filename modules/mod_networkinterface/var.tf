046868ff8c49a554d2dfb5658ce6e419f5382859
ashusharmaa897 <ashusharmaa897@outlook.com>
2024-02-10 16:50:58 +0000

    Updated var.tf

--- modules/mod_networkinterface/var.tf
+++ modules/mod_networkinterface/var.tf
@@ -2,3 +2,15 @@ variable "interface_name" {
     type = string
   default = "my-nic"
 }
+
+variable "output_rg_location" {
+  type= string
+}
+
+variable "output_rg_name" {
+  type = string
+}
+
+variable "output_subnet_id" {
+  type= string
+}
